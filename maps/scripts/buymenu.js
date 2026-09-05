41254b03ac61330d96029e000bd1e1bb1c6760e2
nucleicacid <105178916+nucleicacid@users.noreply.github.com>
2026-09-05 19:25:15 -0400

    Add all weapons

--- maps/scripts/buymenu.js
+++ maps/scripts/buymenu.js
@@ -1,10 +1,46 @@
 import { CSDamageFlags, CSDamageTypes, CSInputs, CSPlayerController, Entity, Instance } from "cs_script/point_script";
 
 const weapons = new Map([
+  // Pistols
+  ["glock", 200],
+  ["hkp2000", 200],
+  ["usp_silencer", 200],
+  ["p250", 300],
+  ["fiveseven", 500],
+  ["tec9", 500],
+  ["cz75a", 500],
+  ["elite", 300],
+  ["deagle", 700],
+  ["revolver", 600],
+  // SMGs
+  ["mac10", 1050],
+  ["mp9", 1250],
+  ["mp7", 1500],
+  ["mp5sd", 1500],
+  ["ump45", 1200],
+  ["p90", 2350],
+  ["bizon", 1400],
+  // Rifles
+  ["galilar", 1800],
+  ["famas", 2050],
   ["ak47", 2700],
-  ["m4a1_silencer", 2900],
   ["m4a1", 2900],
+  ["m4a1_silencer", 2900],
+  ["ssg08", 1700],
+  ["sg556", 3000],
+  ["aug", 3300],
   ["awp", 4750],
+  ["g3sg1", 5000],
+  ["scar20", 5000],
+  // Heavy
+  ["nova", 1050],
+  ["xm1014", 2000],
+  ["sawedoff", 1100],
+  ["mag7", 1300],
+  ["m249", 5200],
+  ["negev", 1700],
+  // Equipment
+  ["taser", 200],
 ]);
 
 let menuLayout = null;
